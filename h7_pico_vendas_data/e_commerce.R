43bd6132d6c69729c62f6fe7359c308041f7dbc4
Fernando Gazoli <fernandogazoli@hotmail.com>
2024-11-11 13:51:20 -0300

    teste de hipóteses finalizado - h7 fernando

--- h7_pico_vendas_data/e_commerce.R
+++ h7_pico_vendas_data/e_commerce.R
@@ -11,9 +11,10 @@ dados_carrinho
 dados_clientes <- read_csv("_data//clean//e_commerce_clientes.csv")
 dados_clientes
 
-
 dados_pedidos <- read_csv("_data//clean//e_commerce_pedidos.csv")
 dados_pedidos
+
+
 
 
 # Transformando os dados para datas, contando a frequência das compras por mês para identificarmos picos respondendo à pergunta solicitada.
@@ -32,7 +33,78 @@ dados_pedidos
     y = "Número de Ocorrências"
    ) +
    theme_minimal()
-   
- # O gráfico mostra um pico de vendas no mês de Junho, onde comemoramos o dia dos namorados, dando o insight de que datas comemorativas estão ligadas a vendas de determinados produtos.
-   
  
+
+   
+ # Criando vetor com datas especiais  
+ 
+  datas_especiais <- c(
+    "01/01/2024",
+    "06/01/2024",
+    "12/02/2024",
+    "13/02/2024",
+    "08/03/2024",
+    "19/03/2024",
+    "29/03/2024",
+    "31/03/2024",
+    "19/04/2024",
+    "21/04/2024",
+    "22/04/2024",
+    "01/05/2024",
+    "02/05/2024",
+    "14/05/2024",
+    "30/05/2024",
+    "12/06/2024",
+    "13/06/2024",
+    "24/06/2024",
+    "29/06/2024",
+    "02/07/2024",
+    "26/07/2024",
+    "02/08/2024",
+    "11/08/2024",
+    "15/08/2024",
+    "07/09/2024",
+    "20/09/2024",
+    "21/09/2024",
+    "12/10/2024",
+    "15/10/2024",
+    "31/10/2024",
+    "01/11/2024",
+    "02/11/2024",
+    "15/11/2024",
+    "20/11/2024",
+    "08/12/2024",
+    "25/12/2024",
+    "31/12/2024"
+      )
+  
+dados_tratados <- dados_pedidos |>
+  mutate(data_corrigida = format(as.Date(dmy_hm(data_pagamento)), "%d/%m/%Y")) |>
+  select(data_corrigida, total_pago) |>
+  filter(!is.na(data_corrigida)) |>
+  # Agrupando por data
+  group_by(data_corrigida) |>
+  summarise(
+    total_pago = sum(total_pago, na.rm = TRUE)
+  )
+ 
+dados_datas_especiais <- dados_tratados |>
+  filter(data_corrigida %in% datas_especiais)
+
+ dados_fora_datas_especiais <- dados_tratados |>
+   filter(!(data_corrigida %in% datas_especiais))
+
+# Testando hipótese de que datas especiais é maior que datas "normais" do calendário
+
+wilcox.test(
+  x = dados_datas_especiais$total_pago,
+  y = dados_fora_datas_especiais$total_pago,
+  alternative = "greater"
+)
+ 
+#  Dada a limitação do tamanho da amostra, podemos considerar um nível de confiança de 90% (ou seja, um valor p de 0,10) para interpretar o resultado. 
+ # Com isso em mente, o valor p obtido (0,06413) agora é considerado estatisticamente significativo, já que é menor que 0,10. 
+ # Portanto, temos uma evidência suficiente, com um nível de confiança de 90%, para rejeitar a hipótese nula.
+
+# Isso indica que a mediana de dados_datas_especiais$total_pago é provavelmente maior que a de dados_fora_datas_especiais$total_pago, sugerindo que, em datas especiais, o total pago tende a ser maior do que fora dessas datas. 
+# No entanto, essa conclusão deve ser vista com cautela devido ao tamanho reduzido da amostra.
